516e39b20440e8ee36718d298626f369719f719b
logan-lieou <logan.lieou@gmail.com>
2020-03-28 21:50:56 -0500

    inital commit

---
+++ polybar/launch.sh
@@ -0,0 +1,9 @@
+llall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar top &
+
+echo "Polybar launched..."
